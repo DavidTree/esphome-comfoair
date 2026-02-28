34b62fc6bb4dab205d86df620326706ca0fc7672
David Tree <darmilatron@protonmail.com>
2026-02-28 23:41:28 +0000

    Fix feature flag for current temperature support

--- components/comfoair/comfoair.h
+++ components/comfoair/comfoair.h
@@ -88,7 +88,7 @@ public:
   climate::ClimateTraits traits() override {
     auto traits = climate::ClimateTraits();
     //traits.set_supports_current_temperature(true);
-    traits.add_feature_flags(CLIMATE_SUPPORTS_CURRENT_TEMPERATURE)
+    traits.add_feature_flags(climate::CLIMATE_SUPPORTS_CURRENT_TEMPERATURE)
     traits.set_visual_min_temperature(COMFOAIR_MIN_SUPPORTED_TEMP);
     traits.set_visual_max_temperature(COMFOAIR_MAX_SUPPORTED_TEMP);
     traits.set_visual_temperature_step(COMFOAIR_SUPPORTED_TEMP_STEP);
